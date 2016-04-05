ce76702b8de16df91866a9e4dc412201e4f0ae89
ngzHappy <819869472@qq.com>
2016-04-05 15:58:20 +0800

    +opencv pca test

--- opencv_data/opencv_pca/run.cpp
+++ opencv_data/opencv_pca/run.cpp
@@ -44,18 +44,18 @@ void draw_axis_array_(
 
     QPainterPath path_;
     path_.moveTo(0,0);
-    path_.lineTo( pox1_x,pox1_y );
-    path_.lineTo( pox1_x-dy1*array_weight,pox1_y+dx1*array_weight );
-    path_.lineTo( pox1_x+dx1*array_len,pox1_y+dy1*array_len );
-    path_.lineTo( pox1_x+dy1*array_weight,pox1_y-dx1*array_weight );
-    path_.lineTo( pox1_y,pox1_y  );
+    path_.lineTo(pox1_x,pox1_y);
+    path_.lineTo(pox1_x-dy1*array_weight,pox1_y+dx1*array_weight);
+    path_.lineTo(pox1_x+dx1*array_len,pox1_y+dy1*array_len);
+    path_.lineTo(pox1_x+dy1*array_weight,pox1_y-dx1*array_weight);
+    path_.lineTo(pox1_y,pox1_y);
 
     path_.moveTo(0,0);
-    path_.lineTo( pox2_x,pox2_y );
-    path_.lineTo( pox2_x-dy2*array_weight,pox2_y+dx2*array_weight );
-    path_.lineTo( pox2_x+dx2*array_len,pox2_y+dy2*array_len );
-    path_.lineTo( pox2_x+dy2*array_weight,pox2_y-dx2*array_weight );
-    path_.lineTo( pox2_x,pox2_y  );
+    path_.lineTo(pox2_x,pox2_y);
+    path_.lineTo(pox2_x-dy2*array_weight,pox2_y+dx2*array_weight);
+    path_.lineTo(pox2_x+dx2*array_len,pox2_y+dy2*array_len);
+    path_.lineTo(pox2_x+dy2*array_weight,pox2_y-dx2*array_weight);
+    path_.lineTo(pox2_x,pox2_y);
 
     painter->setBrush(QColor(255,255,255,222));
     painter->setPen(QPen(QColor(0,0,0,222),1.2));
@@ -74,20 +74,43 @@ extern void run(OpenCVWindow * window) {
     cv::Mat eigenvectors;
     cv::PCACompute(dataWrap,mean,eigenvectors);
 
-    /*绘制*/ 
-    OpenCVScatterItem * item_ =
-        window->insertScatter(data.cbegin(),data.cend());
-    std::shared_ptr< std::function<void(QPainter *)> > c_draw_{
-        new std::function<void(QPainter *)>{
-        [eigenvectors](QPainter * painter) {
-        draw_axis_array_(painter,
-            eigenvectors.at<double>(0,0),eigenvectors.at<double>(0,1),
-            eigenvectors.at<double>(1,0),eigenvectors.at<double>(1,1),
-            100,
-            30,15);
-    }}};
-    item_->setCentrePointPaint(c_draw_);
-    item_->setWindowTitle(u8"绘制特征向量"_qs);
+    /*绘制*/
+    {
+        OpenCVScatterItem * item_=
+            window->insertScatter(data.cbegin(),data.cend());
+        std::shared_ptr< std::function<void(QPainter *)> > c_draw_{
+            new std::function<void(QPainter *)>{
+            [eigenvectors](QPainter * painter) {
+            draw_axis_array_(painter,
+                eigenvectors.at<double>(0,0),eigenvectors.at<double>(0,1),
+                eigenvectors.at<double>(1,0),eigenvectors.at<double>(1,1),
+                100,
+                30,15);
+        }} };
+        item_->setCentrePointPaint(c_draw_);
+        item_->setWindowTitle(u8"绘制特征向量"_qs);
+    }
+
+    cv::Mat projectAns;
+    /*绘制投影*/
+    {
+        cv::PCAProject(dataWrap,mean,eigenvectors,projectAns);
+        auto begin_=reinterpret_cast<cv::Point2d *>(projectAns.data);
+        auto end_=begin_+projectAns.rows;
+        OpenCVScatterItem * item_=window->insertScatter(begin_,end_);
+        item_->setWindowTitle(u8"绘制投影后的数据"_qs);
+        item_->resize(512,20);
+    }
+
+    cv::Mat backProjectAns;
+    /*绘制反投影数据*/
+    {
+        cv::PCABackProject(projectAns,mean,eigenvectors,backProjectAns);
+        auto begin_=reinterpret_cast<cv::Point2d *>(backProjectAns.data);
+        auto end_=begin_+backProjectAns.rows;
+        OpenCVScatterItem * item_=window->insertScatter(begin_,end_);
+        item_->setWindowTitle(u8"绘制反投影后的数据"_qs);
+    }
 
 }
 

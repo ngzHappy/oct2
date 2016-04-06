a7b44a1444a0e1a1c4d3f8e49a357e411e197e76
ngzHappy <819869472@qq.com>
2016-04-06 14:24:42 +0800

    +pyrup final

--- opencv_imgproc/opencv_pyrUp/run.cpp
+++ opencv_imgproc/opencv_pyrUp/run.cpp
@@ -4,33 +4,21 @@
 
 extern void run(OpenCVWindow * window) {
 
-    /*测试图片显示*/
-    {
-        intptr_t count_=0;
-        const auto images_names=
-            CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
-
-        for (const auto & image_name:images_names) {
-            window->insertImage(QImage(image_name))
-                ->setWindowTitle(u8"第%1幅图片"_qs.arg(++count_));
-        }
+    intptr_t count_=0;
+    /*从配置文件中获得输入图片*/
+    const auto images_names=
+        CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
+    /*进行变换*/
+    for (const auto & image_name:images_names) {
+        ++count_;
+        cv::Mat mat=OpenCVUtility::tryRead(QImage(image_name));
+        window->insertImage(OpenCVUtility::tryRead(mat))
+            ->setWindowTitle(u8"第%1幅原始图片"_qs.arg(count_));
+        cv::Mat ans;
+        cv::pyrUp( mat,ans,mat.size()*2 );
+        window->insertImage(OpenCVUtility::tryRead(ans))
+            ->setWindowTitle(u8"第%1幅图片"_qs.arg(count_));
     }
-    /*测试柱状图*/
-    window->insertHist({ 1,2,3,4,5 })->setWindowTitle(u8"柱状图"_qs);
-    /*测试散点图*/
-    auto scatter=window->insertScatter({ {0,0},{1,1},{2,2} });
-    scatter->setCentrePointPaint(
-        std::shared_ptr< std::function<void(QPainter *)> >(
-        new std::function<void(QPainter *)>{
-        [](QPainter * painter) {
-        painter->setBrush(Qt::transparent);
-        painter->setPen(QPen(QColor(0,0,0),1));
-        painter->drawRect(QRect{-10,-10,20,20});
-    }
-    }
-        )
-        );
-    scatter->setWindowTitle(u8"散点图"_qs);
 
 }
 

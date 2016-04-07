791122b1798ea68d82c0f8ca93f4963ae5e76e15
ngzHappy <819869472@qq.com>
2016-04-07 12:20:46 +0800

    +test data

--- opencv_point_algorithm/opencv_approxPolyDP/opencv_approxPolyDP_run.cpp
+++ opencv_point_algorithm/opencv_approxPolyDP/opencv_approxPolyDP_run.cpp
@@ -2,35 +2,57 @@
 #include <OpenCVUtility.hpp>
 #include <opencv_application_configuration_file.hpp>
 
+namespace {
+
+inline double pi_() {static double pi__=std::atan(1.0)*4;
+    return pi__;
+}
+
+inline double pi2_() {static double pi_2_=pi_()*2;
+    return pi_2_;
+}
+
+constexpr inline float to_float(long double v)
+{return static_cast<float>(v);}
+
+inline std::vector<cv::Point2f>
+genRandPoint() {
+    enum { SIZE=100 };
+    std::vector<cv::Point2f> ans;
+    ans.reserve(SIZE);
+    const double half_pi_=pi_()/2;
+    const double step_=pi2_()/6;
+    const std::vector<cv::Point2f> test_triangle{
+        {std::sin(to_float(half_pi_)),std::cos(to_float(half_pi_))},
+        {std::sin(to_float(half_pi_+step_)),std::cos(to_float(half_pi_+step_))},
+        {std::sin(to_float(half_pi_+step_*2)),std::cos(to_float(half_pi_+step_*2))},
+        {std::sin(to_float(half_pi_+step_*3)),std::cos(to_float(half_pi_+step_*3))},
+        {std::sin(to_float(half_pi_+step_*4)),std::cos(to_float(half_pi_+step_*4))},
+        {std::sin(to_float(half_pi_+step_*5)),std::cos(to_float(half_pi_+step_*5))},
+    };
+    for (std::intptr_t i=0; i<SIZE; ++i) {
+    rand_begin_:
+        const double x_=std::sin(double(std::rand()))*0.707;
+        const double y_=std::cos(double(std::rand()))*0.707;
+        if (cv::pointPolygonTest(test_triangle,cv::Point2f(x_,y_),false)<0) {
+            goto rand_begin_;
+        }
+        ans.emplace_back(
+            static_cast<float>(x_+y_),
+            static_cast<float>(x_-y_));
+    }
+    return std::move(ans);
+}
+
+}
+
 extern void run(OpenCVWindow * window) {
 
-    /*测试图片显示*/
-    {
-        intptr_t count_=0;
-        const auto images_names=
-            CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
+    std::vector<cv::Point2f> points_input=
+        genRandPoint();
 
-        for (const auto & image_name:images_names) {
-            window->insertImage(QImage(image_name))
-                ->setWindowTitle(u8"第%1幅图片"_qs.arg(++count_));
-        }
-    }
-    /*测试柱状图*/
-    window->insertHist({ 1,2,3,4,5 })->setWindowTitle(u8"柱状图"_qs);
-    /*测试散点图*/
-    auto scatter=window->insertScatter({ {0,0},{1,1},{2,2} });
-    scatter->setCentrePointPaint(
-        std::shared_ptr< std::function<void(QPainter *)> >(
-        new std::function<void(QPainter *)>{
-        [](QPainter * painter) {
-        painter->setBrush(Qt::transparent);
-        painter->setPen(QPen(QColor(0,0,0),1));
-        painter->drawRect(QRect{-10,-10,20,20});
-    }
-    }
-        )
-        );
-    scatter->setWindowTitle(u8"散点图"_qs);
+    auto * item_ = window->insertScatter(points_input.begin(),points_input.end());
+    item_->setWindowTitle(u8"外接三角形"_qs);
 
 }
 

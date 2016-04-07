8b41a272d70399258e42be0f5868d1f8c49f75c7
ngzHappy <819869472@qq.com>
2016-04-07 18:25:21 +0800

    +find contours final

--- opencv_imgproc/opencv_findContours/opencv_findContours.lua
+++ opencv_imgproc/opencv_findContours/opencv_findContours.lua
@@ -1,9 +1,25 @@
 ﻿--[[lua--]]
 application={
         input_images={
-                "images:000001",
-                "images:000002",
-                "images:000003",
+                [[images:030000]],
+                [[images:030019]],
+--              [[images:030002]],
+--              [[images:030003]],
+--              [[images:030004]],
+--              [[images:030005]],
+--              [[images:030006]],
+--              [[images:030007]],
+--              [[images:030008]],
+--              [[images:030009]],
+--              [[images:030010]],
+--              [[images:030011]],
+--              [[images:030012]],
+--              [[images:030013]],
+--              [[images:030014]],
+--              [[images:030015]],
+--              [[images:030016]],
+--              [[images:030017]],
+--              [[images:030018]],
         }
 }
 
--- opencv_imgproc/opencv_findContours/opencv_findContours_run.cpp
+++ opencv_imgproc/opencv_findContours/opencv_findContours_run.cpp
@@ -7,30 +7,89 @@ extern void run(OpenCVWindow * window) {
     /*测试图片显示*/
     {
         intptr_t count_=0;
+
+        /*从配置文件读入图片路径*/
         const auto images_names=
             CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
 
         for (const auto & image_name:images_names) {
-            window->insertImage(QImage(image_name))
+            QImage input_image_=QImage(image_name);
+            window->insertImage(input_image_)
                 ->setWindowTitle(u8"第%1幅图片"_qs.arg(++count_));
+            input_image_=
+                input_image_.convertToFormat(QImage::Format_Grayscale8);
+            cv::Mat mat=OpenCVUtility::tryRead(input_image_);
+            std::vector<std::vector<cv::Point>> ans;
+
+            /*查找轮廓*/
+            cv::findContours(
+                mat,ans,
+                cv::RETR_EXTERNAL,
+                cv::CHAIN_APPROX_SIMPLE 
+                );
+
+           if (ans.empty()) { continue; }
+
+           /*将轮廓按照从大到小排列*/
+           std::sort(ans.begin(),ans.end(),
+               [](const std::vector<cv::Point> & l,
+                  const std::vector<cv::Point> & r) {
+               return r.size()<l.size();
+           });
+           if (ans[0].empty()) { continue; }
+
+           const auto height_=input_image_.height();
+           const auto width_=input_image_.width();
+
+           /*插入第一个轮廓*/
+           QList<QPointF> data_;
+           data_.reserve( ans[0].size() );
+           for (const auto & i:ans[0]) {
+               data_.push_back(QPointF(i.x,height_-i.y));
+           }
+
+           auto * item_=window->insertLineSeries(data_);
+           item_->getLineSeries()->append(ans[0].begin()->x,
+               height_-(ans[0].begin()->y));
+          
+           /*设置坐标系大小与图片一致*/
+           item_->getChart()->axisX()->setRange(0,width_);
+           item_->getChart()->axisY()->setRange(0,height_);
+
+           item_->resize(width_*1.3,height_*1.3);
+
+           /*插入剩余轮廓*/
+           auto pos_=ans.begin();
+           for ( ++pos_;pos_!=ans.end();++pos_) {
+               if (pos_->empty()) { continue; }
+               data_.clear();
+               for (const auto & i:*(pos_)) {
+                   data_.push_back(QPointF(i.x,height_-i.y));
+               }
+               data_.push_back(
+                   QPointF(pos_->begin()->x,
+                   height_-pos_->begin()->y)
+                   );
+               QtCharts::QLineSeries * series=new QtCharts::QLineSeries;
+               series->append( data_ );
+               item_->getChart()->addSeries(series);
+               item_->getChart()->setAxisX(
+                   item_->getChart()->axisX(item_->getLineSeries()),
+                   series
+                   );
+               item_->getChart()->setAxisY(
+                   item_->getChart()->axisY(item_->getLineSeries()),
+                   series
+                   );
+               series->setPen(item_->getLineSeries()->pen());
+               series->setBrush(item_->getLineSeries()->brush());
+           }
+
+           /*设置轮廓标题*/
+           item_->setWindowTitle(u8"第%1幅图片轮廓"_qs.arg(count_));
         }
     }
-    /*测试柱状图*/
-    window->insertHist({ 1,2,3,4,5 })->setWindowTitle(u8"柱状图"_qs);
-    /*测试散点图*/
-    auto scatter=window->insertScatter({ {0,0},{1,1},{2,2} });
-    scatter->setCentrePointPaint(
-        std::shared_ptr< std::function<void(QPainter *)> >(
-        new std::function<void(QPainter *)>{
-        [](QPainter * painter) {
-        painter->setBrush(Qt::transparent);
-        painter->setPen(QPen(QColor(0,0,0),1));
-        painter->drawRect(QRect{-10,-10,20,20});
-    }
-    }
-        )
-        );
-    scatter->setWindowTitle(u8"散点图"_qs);
+  
 
 }
 

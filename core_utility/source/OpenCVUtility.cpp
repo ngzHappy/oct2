02a5db631cb6448e1fa3e9cbb0fb04e636716b29
ngzHappy <819869472@qq.com>
2016-04-07 19:24:22 +0800

    -bug

--- core_utility/source/OpenCVUtility.cpp
+++ core_utility/source/OpenCVUtility.cpp
@@ -18,12 +18,11 @@ void OpenCVUtility::construct() {
 
 namespace {
 
-void deleteAny( QVariant * data_ ) {
+void deleteAny(QVariant * data_) {
     delete data_;
 }
 
-class StdMatAllocator : public cv::MatAllocator
-{
+class StdMatAllocator : public cv::MatAllocator {
 public:
     cv::UMatData* allocate(
         int dims,
@@ -32,28 +31,27 @@ public:
         void* data0,
         size_t* step,
         int /*flags*/,
-        cv::UMatUsageFlags /*usageFlags*/) const{
+        cv::UMatUsageFlags /*usageFlags*/) const {
 
         using namespace cv;
-        size_t total = CV_ELEM_SIZE(type);
-        for( int i = dims-1; i >= 0; i-- )
-        {
-            if( step ){
-                if( data0 && step[i] != CV_AUTOSTEP ){
-                    CV_Assert(total <= step[i]);
-                    total = step[i];
+        size_t total=CV_ELEM_SIZE(type);
+        for (int i=dims-1; i>=0; i--) {
+            if (step) {
+                if (data0 && step[i]!=CV_AUTOSTEP) {
+                    CV_Assert(total<=step[i]);
+                    total=step[i];
                 }
                 else {
                     step[i]=total;
                 }
             }
-            total *= sizes[i];
+            total*=sizes[i];
         }
 
-        uchar* data = data0 ? (uchar*)data0 : (uchar*)fastMalloc(total);
-        UMatData* u = new UMatData(this);
-        u->data = u->origdata = data;
-        u->size = total;
+        uchar* data=data0?(uchar*)data0:(uchar*)fastMalloc(total);
+        UMatData* u=new UMatData(this);
+        u->data=u->origdata=data;
+        u->size=total;
         if (data0) { u->flags|=UMatData::USER_ALLOCATED; }
 
         return u;
@@ -62,29 +60,29 @@ public:
     bool allocate(
         cv::UMatData* u,
         int /*accessFlags*/,
-        cv::UMatUsageFlags /*usageFlags*/) const{
-        if(!u) return false;
+        cv::UMatUsageFlags /*usageFlags*/) const {
+        if (!u) return false;
         return true;
     }
 
-    void deallocate(cv::UMatData* u) const{
+    void deallocate(cv::UMatData* u) const {
         using namespace cv;
-        if(!u) return;
+        if (!u) return;
 
-        CV_Assert(u->urefcount == 0);
-        CV_Assert(u->refcount == 0);
+        CV_Assert(u->urefcount==0);
+        CV_Assert(u->refcount==0);
 
-        if( !(u->flags & UMatData::USER_ALLOCATED) ){
+        if (!(u->flags & UMatData::USER_ALLOCATED)) {
             fastFree(u->origdata);
-            u->origdata = 0;
+            u->origdata=0;
         }
 
         if (u->userdata) {
             //assert( u->handle == &deleteAny );
             deleteAny(reinterpret_cast<QVariant *>(u->userdata));
-//#if defined(_DEBUG)
-//            qDebug()<<"destory qimage "<<__func__;
-//#endif
+            //#if defined(_DEBUG)
+            //            qDebug()<<"destory qimage "<<__func__;
+            //#endif
         }
 
         delete u;
@@ -93,13 +91,13 @@ public:
 
 }
 
-namespace{
+namespace {
 static cv::MatAllocator * stdMalloc=nullptr;
 }
 
 cv::MatAllocator * OpenCVUtility::getReaderAllocator() {
     if (stdMalloc) { return stdMalloc; }
-    stdMalloc = new StdMatAllocator;
+    stdMalloc=new StdMatAllocator;
     qAddPostRoutine([]() {
         delete stdMalloc;
         stdMalloc=0;
@@ -114,8 +112,8 @@ static inline void __construct() {
         QImage * image_=new QImage(":/i_m_a_g_e_s_/images/000000.png");
         auto width_=image_->width(); (void)width_;
         auto height_=image_->height(); (void)height_;
-        assert( width_>0 );
-        assert( height_>0 );
+        assert(width_>0);
+        assert(height_>0);
         delete image_;
     }
     {
@@ -139,57 +137,57 @@ QImage::Format format_change_(QImage::Format format_) {
         case QImage::Format_MonoLSB:break;
         case QImage::Format_Indexed8:break;
         case QImage::Format_RGB32: {
-            image_= QImage::Format_RGB888 ;
+            image_=QImage::Format_RGB888;
         }break;
         case QImage::Format_ARGB32: {
-            image_= QImage::Format_RGBA8888 ;
+            image_=QImage::Format_RGBA8888;
         }break;
-        case QImage::Format_ARGB32_Premultiplied:{
-            image_= QImage::Format_RGBA8888 ;
+        case QImage::Format_ARGB32_Premultiplied: {
+            image_=QImage::Format_RGBA8888;
         }break;
-        case QImage::Format_RGB16:{
-            image_= QImage::Format_RGB888 ;
+        case QImage::Format_RGB16: {
+            image_=QImage::Format_RGB888;
         }break;
-        case QImage::Format_ARGB8565_Premultiplied:{
-            image_= QImage::Format_RGBA8888 ;
+        case QImage::Format_ARGB8565_Premultiplied: {
+            image_=QImage::Format_RGBA8888;
         }break;
-        case QImage::Format_RGB666:{
-            image_= QImage::Format_RGB888 ;
+        case QImage::Format_RGB666: {
+            image_=QImage::Format_RGB888;
         }break;
         case QImage::Format_ARGB6666_Premultiplied: {
-            image_= QImage::Format_RGBA8888 ;
+            image_=QImage::Format_RGBA8888;
         }break;
         case QImage::Format_RGB555: {
-            image_= QImage::Format_RGB888 ;
+            image_=QImage::Format_RGB888;
         }break;
         case QImage::Format_ARGB8555_Premultiplied: {
-            image_= QImage::Format_RGBA8888 ;
+            image_=QImage::Format_RGBA8888;
         }break;
         case QImage::Format_RGB888: break;
         case QImage::Format_RGB444: {
-            image_= QImage::Format_RGB888 ;
+            image_=QImage::Format_RGB888;
         }break;
         case QImage::Format_ARGB4444_Premultiplied: {
-            image_= QImage::Format_RGBA8888 ;
+            image_=QImage::Format_RGBA8888;
         }break;
         case QImage::Format_RGBX8888: {
-            image_= QImage::Format_RGB888 ;
+            image_=QImage::Format_RGB888;
         }break;
         case QImage::Format_RGBA8888: {}break;
         case QImage::Format_RGBA8888_Premultiplied: {
-            image_= QImage::Format_RGBA8888 ;
+            image_=QImage::Format_RGBA8888;
         }break;
         case QImage::Format_BGR30: {
-            image_= QImage::Format_RGB888 ;
+            image_=QImage::Format_RGB888;
         }break;
         case QImage::Format_A2BGR30_Premultiplied: {
-            image_= QImage::Format_RGBA8888 ;
+            image_=QImage::Format_RGBA8888;
         }break;
         case QImage::Format_RGB30: {
-            image_= QImage::Format_RGB888 ;
+            image_=QImage::Format_RGB888;
         }break;
         case QImage::Format_A2RGB30_Premultiplied: {
-            image_= QImage::Format_RGBA8888 ;
+            image_=QImage::Format_RGBA8888;
         }break;
         case QImage::Format_Alpha8:break;
         case QImage::Format_Grayscale8:break;
@@ -201,8 +199,8 @@ QImage::Format format_change_(QImage::Format format_) {
 }
 
 cv::Mat OpenCVUtility::read(const QString & string_) {
-    QImage image_(string_) ;
-    return read( image_ );
+    QImage image_(string_);
+    return read(image_);
 }
 
 cv::Mat OpenCVUtility::read(const QImage & image__) {
@@ -212,15 +210,15 @@ cv::Mat OpenCVUtility::read(const QImage & image__) {
     /*add a ref*/
     QImage image_=image__;
 
-    QImage::Format format_ = image_.format();
-    format_=format_change_( format_ );
-    image_ = image_.convertToFormat(format_);
-    format_ = image_.format();
+    QImage::Format format_=image_.format();
+    format_=format_change_(format_);
+    image_=image_.convertToFormat(format_);
+    format_=image_.format();
 
     /*创建数据的独立副本*/
     //image_.detach();
 
-    if ( format_ == QImage::Format_RGB888 ) {
+    if (format_==QImage::Format_RGB888) {
         cv::Mat xmat_(
             image_.height(),
             image_.width(),
@@ -228,18 +226,19 @@ cv::Mat OpenCVUtility::read(const QImage & image__) {
             const_cast<uchar *>(image_.constBits()),
             image_.bytesPerLine()
             );
-        assert( xmat_.u == nullptr );
+        assert(xmat_.u==nullptr);
+        assert(xmat_.data);
         xmat_.u=getReaderAllocator()
-            ->allocate(0,nullptr,0,image_.bits(),nullptr,0,cv::USAGE_DEFAULT);
-        assert( xmat_.u->currAllocator == getReaderAllocator() );
-        assert( xmat_.u->userdata == nullptr );
-        assert( xmat_.u->handle == nullptr );
+            ->allocate(0,nullptr,0,xmat_.data,nullptr,0,cv::USAGE_DEFAULT);
+        assert(xmat_.u->currAllocator==getReaderAllocator());
+        assert(xmat_.u->userdata==nullptr);
+        assert(xmat_.u->handle==nullptr);
         //xmat_.u->handle = reinterpret_cast<void *>( OpenCVUtility::getHandle() );
         xmat_.u->userdata=new QVariant(QImage(std::move(image_)));
         xmat_.u->refcount=1;
         return std::move(xmat_);
     }
-    else if ( format_ == QImage::Format_RGBA8888 ) {
+    else if (format_==QImage::Format_RGBA8888) {
         cv::Mat xmat_(
             image_.height(),
             image_.width(),
@@ -247,12 +246,13 @@ cv::Mat OpenCVUtility::read(const QImage & image__) {
             const_cast<uchar *>(image_.constBits()),
             image_.bytesPerLine()
             );
-        assert( xmat_.u == nullptr );
+        assert(xmat_.u==nullptr);
+        assert(xmat_.data);
         xmat_.u=getReaderAllocator()
-            ->allocate(0,nullptr,0,image_.bits(),nullptr,0,cv::USAGE_DEFAULT);
-        assert( xmat_.u->currAllocator == getReaderAllocator() );
-        assert( xmat_.u->userdata == nullptr );
-        assert( xmat_.u->handle == nullptr );
+            ->allocate(0,nullptr,0,xmat_.data,nullptr,0,cv::USAGE_DEFAULT);
+        assert(xmat_.u->currAllocator==getReaderAllocator());
+        assert(xmat_.u->userdata==nullptr);
+        assert(xmat_.u->handle==nullptr);
         //xmat_.u->handle= reinterpret_cast<void *>(OpenCVUtility::getHandle());
         xmat_.u->userdata=new QVariant(QImage(std::move(image_)));
         xmat_.u->refcount=1;
@@ -267,12 +267,13 @@ cv::Mat OpenCVUtility::read(const QImage & image__) {
                 const_cast<uchar *>(image_.constBits()),
                 image_.bytesPerLine()
                 );
-            assert( xmat_.u == nullptr );
+            assert(xmat_.u==nullptr);
+            assert(xmat_.data);
             xmat_.u=getReaderAllocator()
-                ->allocate(0,nullptr,0,image_.bits(),nullptr,0,cv::USAGE_DEFAULT);
-            assert( xmat_.u->currAllocator == getReaderAllocator() );
-            assert( xmat_.u->userdata == nullptr );
-            assert( xmat_.u->handle == nullptr );
+                ->allocate(0,nullptr,0,xmat_.data,nullptr,0,cv::USAGE_DEFAULT);
+            assert(xmat_.u->currAllocator==getReaderAllocator());
+            assert(xmat_.u->userdata==nullptr);
+            assert(xmat_.u->handle==nullptr);
             //xmat_.u->handle= reinterpret_cast<void *>(OpenCVUtility::getHandle());
             xmat_.u->userdata=new QVariant(QImage(std::move(image_)));
             xmat_.u->refcount=1;
@@ -285,13 +286,13 @@ cv::Mat OpenCVUtility::read(const QImage & image__) {
 
 }
 
-namespace  {
-void handle_qimage_(void * v){
-    delete reinterpret_cast<cv::Mat *>( v );
+namespace {
+void handle_qimage_(void * v) {
+    delete reinterpret_cast<cv::Mat *>(v);
 }
 }
 
-OpenCVUtility::HandleQImage OpenCVUtility::getHandleQImage(){
+OpenCVUtility::HandleQImage OpenCVUtility::getHandleQImage() {
     return &handle_qimage_;
 }
 
@@ -315,104 +316,106 @@ cv::Mat OpenCVUtility::getInnerOpenCVMat(QImage && v) {
     return getInnerOpenCVMat(value_);
 }
 
-cv::Mat OpenCVUtility::getInnerOpenCVMat(const QImage & image_){
-    QImage::DataPtr data_ = const_cast<QImage &>(image_).data_ptr();
-    if(data_){
-        if( data_->cleanupInfo ){
-            if( data_->cleanupFunction == getHandleQImage() ){
-                return *( reinterpret_cast<cv::Mat *>( data_->cleanupInfo ) );
+cv::Mat OpenCVUtility::getInnerOpenCVMat(const QImage & image_) {
+    QImage::DataPtr data_=const_cast<QImage &>(image_).data_ptr();
+    if (data_) {
+        if (data_->cleanupInfo) {
+            if (data_->cleanupFunction==getHandleQImage()) {
+                return *(reinterpret_cast<cv::Mat *>(data_->cleanupInfo));
             }
         }
     }
     return cv::Mat();
 }
 
-QImage OpenCVUtility::read(const cv::Mat & v){
+QImage OpenCVUtility::read(const cv::Mat & v) {
     {
-        const cv::Mat & tmp = v;
-        if ( v.type() == CV_8UC1 ) {
-            if(tmp.cols>0  && tmp.rows>0 )
+        const cv::Mat & tmp=v;
+        if (v.type()==CV_8UC1) {
+            if (tmp.cols>0&&tmp.rows>0)
                 return QImage(
                     tmp.data,
                     tmp.cols,tmp.rows,
                     tmp.step,
                     QImage::Format_Grayscale8,
                     OpenCVUtility::getHandleQImage(),
-                    new cv::Mat( tmp )
+                    new cv::Mat(tmp)
                     );
             else return QImage();
         }
-        else if(v.type() == CV_8UC3){
-            if(tmp.cols>0  && tmp.rows>0 )
+        else if (v.type()==CV_8UC3) {
+            if (tmp.cols>0&&tmp.rows>0)
                 return QImage(
                     tmp.data,
                     tmp.cols,tmp.rows,
                     tmp.step,
                     QImage::Format_RGB888,
                     OpenCVUtility::getHandleQImage(),
-                    new cv::Mat( tmp )
+                    new cv::Mat(tmp)
                     );
             else return QImage();
         }
-        else if ( v.type() == CV_8UC4 ) {
-            if(tmp.cols>0  && tmp.rows>0 )
+        else if (v.type()==CV_8UC4) {
+            if (tmp.cols>0&&tmp.rows>0)
                 return QImage(
                     tmp.data,
                     tmp.cols,tmp.rows,
                     tmp.step,
                     QImage::Format_RGBA8888,
                     OpenCVUtility::getHandleQImage(),
-                    new cv::Mat( tmp )
+                    new cv::Mat(tmp)
                     );
             else return QImage();
         }
     }
 
     cv::Mat tmp;
-    const auto channels_ = v.channels();
-    if( channels_ == 1 ){
-        v.convertTo( tmp,CV_8UC1 );
-        if(tmp.cols>0  && tmp.rows>0 ){
+    const auto channels_=v.channels();
+    if (channels_==1) {
+        v.convertTo(tmp,CV_8UC1);
+        if (tmp.cols>0&&tmp.rows>0) {
             return QImage(
                         tmp.data,
                         tmp.cols,tmp.rows,
                         tmp.step,
                         QImage::Format_Grayscale8,
                         OpenCVUtility::getHandleQImage(),
-                        new cv::Mat( tmp )
+                        new cv::Mat(tmp)
                         );
         }
-    }else if( channels_ == 3 ){
-        v.convertTo( tmp,CV_8UC3 );
-        if(tmp.cols>0  && tmp.rows>0 )
-        return QImage(
-                    tmp.data,
-                    tmp.cols,tmp.rows,
-                    tmp.step,
-                    QImage::Format_RGB888,
-                    OpenCVUtility::getHandleQImage(),
-                    new cv::Mat( tmp )
-                    );
-    }else if( channels_ == 4 ){
-        v.convertTo( tmp,CV_8UC4 );
-        if(tmp.cols>0  && tmp.rows>0 )
-        return QImage(
-                    tmp.data,
-                    tmp.cols,tmp.rows,
-                    tmp.step,
-                    QImage::Format_RGBA8888,
-                    OpenCVUtility::getHandleQImage(),
-                    new cv::Mat( tmp )
-                    );
+    }
+    else if (channels_==3) {
+        v.convertTo(tmp,CV_8UC3);
+        if (tmp.cols>0&&tmp.rows>0)
+            return QImage(
+                        tmp.data,
+                        tmp.cols,tmp.rows,
+                        tmp.step,
+                        QImage::Format_RGB888,
+                        OpenCVUtility::getHandleQImage(),
+                        new cv::Mat(tmp)
+                        );
+    }
+    else if (channels_==4) {
+        v.convertTo(tmp,CV_8UC4);
+        if (tmp.cols>0&&tmp.rows>0)
+            return QImage(
+                        tmp.data,
+                        tmp.cols,tmp.rows,
+                        tmp.step,
+                        QImage::Format_RGBA8888,
+                        OpenCVUtility::getHandleQImage(),
+                        new cv::Mat(tmp)
+                        );
     }
     return QImage();
 }
 
 QImage OpenCVUtility::getInnerQImage(const cv::Mat & v) {
-    if ( (v.u) && (  v.u->currAllocator == getReaderAllocator() ) ) {
+    if ((v.u)&&(v.u->currAllocator==getReaderAllocator())) {
         if (v.u->userdata) {
             //assert( v.u->handle == OpenCVUtility::getHandle() );
-            return reinterpret_cast<QVariant *>( v.u->userdata )->value<QImage>();
+            return reinterpret_cast<QVariant *>(v.u->userdata)->value<QImage>();
         }
     }
     return QImage();
@@ -420,37 +423,40 @@ QImage OpenCVUtility::getInnerQImage(const cv::Mat & v) {
 
 cv::Mat OpenCVUtility::tryRead(QImage && v) {
     const QImage image_(std::move(v));
-    return tryRead( image_ );
+    return tryRead(image_);
 }
 
 cv::Mat OpenCVUtility::tryRead(const QImage & v) {
     if (v.width()<=0) { return cv::Mat(); }
     if (v.height()<=0) { return cv::Mat(); }
-    cv::Mat ans_=getInnerOpenCVMat( v );
-    if (ans_.rows<=0 || ans_.cols<=0) {
-        return read( v );
+
+    cv::Mat ans_=getInnerOpenCVMat(v);
+
+    if (ans_.rows<=0||ans_.cols<=0) {
+        return read(v);
     }
     return std::move(ans_);
 }
 
 QImage OpenCVUtility::tryRead(cv::Mat && v) {
     const cv::Mat value_(std::move(v));
-    return tryRead( value_ );
+    return tryRead(value_);
 }
 
 QImage OpenCVUtility::tryRead(const cv::Mat & v) {
     if (v.rows<=0) { return QImage(); }
     if (v.cols<=0) { return QImage(); }
-    QImage ans_=getInnerQImage( v );
+
+    QImage ans_=getInnerQImage(v);
 
     if (ans_.width()<=0||ans_.height()<=0) {
-        return read( v );
+        return read(v);
     }
 
     /*roi*/
     if ((ans_.width()!=v.cols)||
         (ans_.height()!=v.rows)) {
-        return read( v );
+        return read(v);
     }
 
     return std::move(ans_);
--- opencv_imgproc/opencv_canny/opencv_canny_run.cpp
+++ opencv_imgproc/opencv_canny/opencv_canny_run.cpp
@@ -12,12 +12,12 @@ extern void run(OpenCVWindow * window) {
     for (const auto & image_name:images_names) {
         ++count_;
         cv::Mat mat=OpenCVUtility::tryRead(QImage(image_name));
-        window->insertImage(OpenCVUtility::tryRead(mat))
+        window->insertImage(OpenCVUtility::tryRead(mat) )
             ->setWindowTitle(u8"第%1幅原始图片"_qs.arg(count_));
         cv::Mat ans;
         cv::blur(mat,ans,{3,3});/*平滑滤波*/
         cv::Canny(ans,ans,100,200);/*canny 边缘检测*/
-        window->insertImage(OpenCVUtility::tryRead(ans))
+        window->insertImage(OpenCVUtility::tryRead(ans) )
             ->setWindowTitle(u8"第%1幅图片"_qs.arg(count_));
     }
 

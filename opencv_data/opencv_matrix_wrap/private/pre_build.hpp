b74e8cb3337fc6aec735f55b021935cab46c2a49
ngzHappy <819869472@qq.com>
2016-04-07 17:04:27 +0800

    cut prebuild

--- opencv_data/opencv_matrix_wrap/private/pre_build.hpp
+++ opencv_data/opencv_matrix_wrap/private/pre_build.hpp
@@ -10,18 +10,6 @@
 
 #include <opencv2/opencv.hpp>
 
-#if defined(QT_CORE_LIB)
-#include <QtCore/QtCore>
-#endif/*~QtCore*/
-
-#if defined(QT_WIDGETS_LIB)
-#include <QtWidgets/QtWidgets>
-#endif/*~QtWidgets*/
-
-#if defined(QT_GUI_LIB)
-#include <QtGui/QtGui>
-#endif/*~QT_GUI_LIB*/
-
 #endif/*~__cplusplus*/
 
 #endif/*~__PRE_v_BUILD__HPP__0x00*/

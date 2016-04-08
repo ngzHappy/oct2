68d0ba09fcc4821405fe9fce6417782b24c34946
ngzHappy <819869472@qq.com>
2016-04-08 14:33:13 +0800

    +change show pos

--- opencv_data/opencv_rect/main.cpp
+++ opencv_data/opencv_rect/main.cpp
@@ -50,9 +50,9 @@ int main(int argc,char ** argv) {
 
     MainWindow * window=new MainWindow;
     window->setAttribute(Qt::WA_DeleteOnClose);
+    window->show();
     run(window->getOpenCVWindow());
     std::cout.flush();
-    window->show();
 
     return app.exec();
 

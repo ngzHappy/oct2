ce32c819844468a99188e177b52b38872cb33bac
ngzHappy <819869472@qq.com>
2016-04-08 15:52:17 +0800

    +delanuay final

--- opencv_point_algorithm/opencv_delaunay/opencv_delaunay_run.cpp
+++ opencv_point_algorithm/opencv_delaunay/opencv_delaunay_run.cpp
@@ -12,8 +12,8 @@ std::vector<cv::Point2f> genPoints() {
     std::vector<cv::Point2f> ans;
     enum {SIZE=10};
     
-    const double x_start_=-1;
-    const double y_start_=-1;
+    const double x_start_=1;
+    const double y_start_=1;
 
     const double step_x=2.0/SIZE;
     const double step_y=2.0/SIZE;
@@ -43,8 +43,32 @@ extern void run(OpenCVWindow * window) {
     auto * item =
         window->insertScatter(points.begin(),points.end());
 
-     
+    cv::Subdiv2D delanunay(cv::Rect(1,1,2,2));
+    delanunay.insert(points);
+    std::vector< cv::Vec6f > ans;
+    delanunay.getTriangleList(ans);
 
+    std::cout<<ans.size()<<std::endl;
+
+    /*绘制结果*/
+    {
+        auto * chart_=item->getChart();
+        auto * axis_x=chart_->axisX(item->getScatterSeries())/*获得x轴*/;
+        auto * axis_y=chart_->axisY(item->getScatterSeries())/*获得y轴*/;
+        const QPen pen(QColor(0,0,0,100),2);
+        for (const auto & i:ans) {
+            QtCharts::QLineSeries * item_=new QtCharts::QLineSeries;
+            item_->append(i[0],i[1])/*第一个点*/;
+            item_->append(i[2],i[3])/*第二个点*/;
+            item_->append(i[4],i[5])/*第三个点*/;
+            item_->append(i[0],i[1])/*闭合三角形*/;
+            chart_->addSeries(item_);
+            chart_->setAxisX(axis_x,item_)/*设置此三角形x轴*/;
+            chart_->setAxisY(axis_y,item_)/*设置此三角形y轴*/;
+            item_->setPen(pen);
+            item_->setPointsVisible(true);
+        }
+    }
     item->setWindowTitle(u8"三角形剖分"_qs);
 }
 

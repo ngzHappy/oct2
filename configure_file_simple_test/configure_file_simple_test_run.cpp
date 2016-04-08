cb8fcbd0cffa53f6c6a3abc244539e6f073f321e
ngzHappy <819869472@qq.com>
2016-04-08 22:48:16 +0800

    +reformat cpp file

--- configure_file_simple_test/configure_file_simple_test_run.cpp
+++ configure_file_simple_test/configure_file_simple_test_run.cpp
@@ -4,7 +4,7 @@
 #include <QtCore/qdebug.h>
 //#include <QtCharts>
 
-extern void run(OpenCVWindow * ) {
+extern void run(OpenCVWindow *) {
 
     /*测试图片*/
     {
@@ -16,50 +16,55 @@ extern void run(OpenCVWindow * ) {
             qDebug()<<in_;
         }
     }
-   
+
     {
         qDebug()<<"data 1d";
-        auto data_1d = CoreUtility::getConfigurationFile()
+        auto data_1d=CoreUtility::getConfigurationFile()
             .getInputData1D();
         for (const auto & i:data_1d) {
             qDebug()<<i;
         }
     }
+
     {
         qDebug()<<"data 2d";
-        auto data_2d =CoreUtility::getConfigurationFile()
+        auto data_2d=CoreUtility::getConfigurationFile()
             .getInputData2D<float,std::vector<cv::Point2f>>();
         for (const auto & i:data_2d) {
             qDebug()<<i.x<<i.y;
         }
     }
+
     {
         qDebug()<<"data 3d";
-        auto data_3d =CoreUtility::getConfigurationFile()
+        auto data_3d=CoreUtility::getConfigurationFile()
             .getInputData3D();
         for (const auto & i:data_3d) {
             qDebug()<<i[0]<<i[1]<<i[2];
         }
     }
+
     {
         qDebug()<<"data 1d Integer";
-        auto data_1d = CoreUtility::getConfigurationFile()
+        auto data_1d=CoreUtility::getConfigurationFile()
             .getInputData1DInteger();
         for (const auto & i:data_1d) {
             qDebug()<<i;
         }
     }
+
     {
         qDebug()<<"data 2d Integer";
-        auto data_2d =CoreUtility::getConfigurationFile()
+        auto data_2d=CoreUtility::getConfigurationFile()
             .getInputData2DInteger();
         for (const auto & i:data_2d) {
             qDebug()<<i.first<<i.second;
         }
     }
+
     {
         qDebug()<<"data 3d Integer";
-        auto data_3d =CoreUtility::getConfigurationFile()
+        auto data_3d=CoreUtility::getConfigurationFile()
             .getInputData3DInteger();
         for (const auto & i:data_3d) {
             qDebug()<<i[0]<<i[1]<<i[2];

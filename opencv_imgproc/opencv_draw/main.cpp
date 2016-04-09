14cd15c4c2fed9a1d0956d8dd62da260630a34cb
ngzHappy <819869472@qq.com>
2016-04-09 11:35:07 +0800

    +change main

--- opencv_imgproc/opencv_draw/main.cpp
+++ opencv_imgproc/opencv_draw/main.cpp
@@ -17,7 +17,7 @@ public:
         this->addVersionOption();
         this->addHelpOption();
         this->addOption(QCommandLineOption("lua","lua configure file",QString(),
-            "opencv_draw.lua"));
+            "opencv_draw"));
     }
 };
 

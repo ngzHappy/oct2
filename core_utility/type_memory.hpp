354df6bec4433045c5597a5818b3dc315c657142
ngzHappy <819869472@qq.com>
2016-04-11 07:33:24 +0800

    +change type_memory

--- core_utility/type_memory.hpp
+++ core_utility/type_memory.hpp
@@ -7,7 +7,8 @@ namespace cct {
 
 template<
     typename _T_,
-    typename _D_
+    typename _D_,
+    typename _S_=void
 >
 inline auto wrap_unique(_T_ * _data_,_D_&&_deleter)->std::unique_ptr<_T_,std::remove_reference_t<_D_>> {
     return std::unique_ptr<_T_,std::remove_reference_t<_D_>>{
@@ -16,7 +17,8 @@ inline auto wrap_unique(_T_ * _data_,_D_&&_deleter)->std::unique_ptr<_T_,std::re
 
 template<
     typename _T_,
-    typename _D_
+    typename _D_,
+    typename _S_=void
 >
 inline auto wrap_shared_ptr(_T_ * _data_,_D_&&_deleter)->std::shared_ptr<_T_> {
     return std::shared_ptr<_T_>(
@@ -26,6 +28,7 @@ inline auto wrap_shared_ptr(_T_ * _data_,_D_&&_deleter)->std::shared_ptr<_T_> {
 typedef void(*deleter_uniqute_ptr_t)(const void *);
 template<
     typename _T_,
+    typename _S_=void,
     typename ... Args
 >
 inline auto make_unique(Args && ... args)->std::unique_ptr<_T_,deleter_uniqute_ptr_t> {
@@ -38,6 +41,7 @@ inline auto make_unique(Args && ... args)->std::unique_ptr<_T_,deleter_uniqute_p
 
 template<
     typename _T_,
+    typename _S_=void,
     typename ... Args
 >
 inline auto instance_unique(Args && ... args)->std::unique_ptr<_T_,deleter_uniqute_ptr_t> {
@@ -76,12 +80,10 @@ inline auto make_shared_ptr_add_deleter(
     Args&&...args
     ) ->std::shared_ptr<_T_> {
     static_assert(std::is_array<_T_>::value==false,"use std::array<type,size_t>");
-    typedef std::remove_const_t<std::remove_reference_t<Deleter>> Deleter_;
-    Deleter_ deleter_0_{ std::forward<Deleter>(deleter) }/*create a copy*/;
     auto data_=make_unique<_T_>(std::forward<Args>(args)...)/*create data*/;
     deleter_uniqute_ptr_t deleter_1_=data_.get_deleter();
     auto deleter_=[deleter_1_=std::move(deleter_1_),
-        deleter_0_=std::move(deleter_0_)
+        deleter_0_=std::forward<Deleter>(deleter)
     ](_T_ * _data) {deleter_0_(_data); deleter_1_(_data); }/*create delete function*/;
     return std::shared_ptr<_T_>(
         data_.release(),
@@ -99,12 +101,10 @@ inline auto instance_shared_ptr_add_deleter(
     Args&&...args
     ) ->std::shared_ptr<_T_> {
     static_assert(std::is_array<_T_>::value==false,"use std::array<type,size_t>");
-    typedef std::remove_const_t<std::remove_reference_t<Deleter>> Deleter_;
-    Deleter_ deleter_0_{ std::forward<Deleter>(deleter) }/*create a copy*/;
     auto data_=instance_unique<_T_>(std::forward<Args>(args)...)/*create data*/;
     deleter_uniqute_ptr_t deleter_1_=data_.get_deleter();
     auto deleter_=[deleter_1_=std::move(deleter_1_),
-        deleter_0_=std::move(deleter_0_)
+        deleter_0_=std::forward<Deleter>(deleter)
     ](_T_ * _data) {deleter_0_(_data); deleter_1_(_data); }/*create delete function*/;
     return std::shared_ptr<_T_>(
         data_.release(),

5fc33b8ca4ae562a1b801b96bfc9d169ff822913
ngzHappy <819869472@qq.com>
2016-04-11 11:05:19 +0800

    +fitline final

--- opencv_point_algorithm/opencv_fitLine/opencv_fitLine_run.cpp
+++ opencv_point_algorithm/opencv_fitLine/opencv_fitLine_run.cpp
@@ -2,7 +2,7 @@
 #include <OpenCVUtility.hpp>
 #include <opencv_application_configuration_file.hpp>
 #include <QtCore/qdebug.h>
-//#include <QtCharts>
+#include <QtCharts>
 
 #include <QtCore/qtextstream.h>
 #include <QtWidgets/qapplication.h>
@@ -19,21 +19,23 @@ int ErrorCallback(
     QString __error_;
 
 #if !defined(NDEBUG)
-    cv::Error::Code estatus= static_cast<cv::Error::Code>(status);
+    cv::Error::Code estatus=static_cast<cv::Error::Code>(status);
     (void)estatus;
 #endif
 
     {
         QTextStream stream_(&__error_);
         stream_
-            <<"function name:"<<func_name
-            <<"error message:"<<err_msg
-            <<"file name:"<<file_name
-            <<"line:"<<line;
+            <<"function name:"<<func_name<<endl
+            <<"error message:"<<err_msg<<endl
+            <<"file name:"<<file_name<<endl
+            <<"line:"<<line<<endl;
     }
 
     if (qApp) {
+        qDebug().noquote()<<__error_;
         QErrorMessage errorBox;
+        errorBox.setModal(true);
         errorBox.showMessage(__error_,"error");
         errorBox.exec();
     }
@@ -51,36 +53,66 @@ static void _set_opencv_error_on_qt_start_up() {
 }
 Q_COREAPP_STARTUP_FUNCTION(_set_opencv_error_on_qt_start_up)
 
-extern void run(OpenCVWindow * window) try{
+extern void run(OpenCVWindow * window) try {
 
-    /*测试图片显示*/
-    {
-        intptr_t count_=0;
-        const auto images_names=
-            CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
+    std::vector<cv::Point2f> varPointsInput;
 
-            for (const auto & image_name:images_names) {
-            window->insertImage(QImage(image_name))
-                ->setWindowTitle(u8"第%1幅图片"_qs.arg(++count_));
-        }
-    }
-    /*测试柱状图*/
-    window->insertHist({ 1,2,3,4,5 })->setWindowTitle(u8"柱状图"_qs);
-    /*测试散点图*/
-    auto scatter=window->insertScatter({ {0,0},{1,1},{2,2} });
-    scatter->setCentrePointPaint(
-        std::shared_ptr< std::function<void(QPainter *)> >(
-        new std::function<void(QPainter *)>{
-        [](QPainter * painter) {
-        painter->setBrush(Qt::transparent);
-        painter->setPen(QPen(QColor(0,0,0),1));
-        painter->drawRect(QRect{-10,-10,20,20});
-    }
-    }
-        )
+    varPointsInput=
+        CoreUtility::getConfigurationFile()
+        .getInputData2D<float,std::vector< cv::Point2f >/**/>();
+
+    auto varItem = window->insertLineSeries(
+        varPointsInput.begin(),
+        varPointsInput.end()
         );
-    scatter->setWindowTitle(u8"散点图"_qs);
 
+    varItem->setWindowTitle(u8"直线拟合"_qs);
+
+    cv::Vec4f varAns;
+    cv::fitLine(
+        varPointsInput/*input*/,
+        varAns/*output*/,
+        cv::DIST_L2/*最小二乘法*/,
+        0,
+        0.01,
+        0.01
+        );
+
+    std::cout
+        <<"dx: "<<varAns[0]<<std::endl
+        <<"dy: "<<varAns[1]<<std::endl
+        <<"x0: "<<varAns[2]<<std::endl
+        <<"y0: "<<varAns[3]<<std::endl;
+    
+    /*
+    -dy*x+dx*y+c=0;
+    c=dy*x0-dx*y0
+    y=(dy*x-c)/dx
+    */
+    auto & varDx=varAns[0];
+    auto & varDy=varAns[1];
+    auto & varX0=varAns[2];
+    auto & varY0=varAns[3];
+    auto varY=
+        [
+            _varC=-((varDy*varX0)-(varDx*varY0)),
+            _varDy=varDy,_varDx=1.0/varDx
+        ](auto argX) {
+        return (_varC+_varDy*argX)*_varDx;
+    };
+
+    if (varPointsInput.empty()) { return; }
+    auto varFirstPointX=varPointsInput.front().x;
+    auto varLastPointX=varPointsInput.rbegin()->x;
+ 
+    auto varChart = varItem->getChart();
+    auto varLineSeriex=new QtCharts::QLineSeries;
+    varLineSeriex->append(varFirstPointX,varY(varFirstPointX));
+    varLineSeriex->append(varLastPointX,varY(varLastPointX));
+    varChart->addSeries(varLineSeriex);
+    varChart->setAxisY(varChart->axisY(varItem->getLineSeries()),varLineSeriex);
+    varChart->setAxisX(varChart->axisX(varItem->getLineSeries()),varLineSeriex);
+    varLineSeriex->setPen(QPen(QColor(1,1,1),2));
 }
 catch (const cv::Exception &e) {
     opencv_exception::error(e);
--- projectMaker/source/_run.cpp
+++ projectMaker/source/_run.cpp
@@ -26,15 +26,16 @@ int ErrorCallback(
     {
         QTextStream stream_(&__error_);
         stream_
-            <<"function name:"<<func_name
-            <<"error message:"<<err_msg
-            <<"file name:"<<file_name
-            <<"line:"<<line;
+            <<"function name:"<<func_name<<endl
+            <<"error message:"<<err_msg<<endl
+            <<"file name:"<<file_name<<endl
+            <<"line:"<<line<<endl;
     }
 
     if (qApp) {
         qDebug().noquote()<<__error_;
         QErrorMessage errorBox;
+        errorBox.setModal(true);
         errorBox.showMessage(__error_,"error");
         errorBox.exec();
     }

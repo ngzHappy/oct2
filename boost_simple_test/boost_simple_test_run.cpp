0b04eaf6f094bc18dd5f373b68c15021bd2e19a8
ngzHappy <819869472@qq.com>
2016-04-12 12:29:39 +0800

    +boost/geometry

--- boost_simple_test/boost_simple_test_run.cpp
+++ boost_simple_test/boost_simple_test_run.cpp
@@ -8,9 +8,37 @@
 
 extern void run(OpenCVWindow * ) try{
 
-    boost::shared_ptr<int> value=boost::make_shared<int>(12);
-    assert( value );
-    assert( *value==12 );
+    {
+        boost::shared_ptr<int> value=boost::make_shared<int>(12);
+        assert(value);
+        assert(*value==12);
+    }
+
+    {
+        boost::pool<> testPool{ 16 };
+        auto data=testPool.malloc();
+        assert(data);
+        (void)data;
+    }
+
+    {
+        boost::circular_buffer<int> buffer{200};
+    }
+
+    {
+        boost::optional<int> value;
+        assert(bool(value)==false);
+        value=12;
+        assert(value);
+    }
+
+    {
+        boost::signals2::signal<void(void)> signal;
+        auto sc= signal.connect([]() {std::cout<<"1"<<std::endl; });
+        signal();
+        sc.disconnect();
+        signal();
+    }
 
 }
 catch (const cv::Exception &e) {
--- core_utility/boost/PartBoost.hpp
+++ core_utility/boost/PartBoost.hpp
@@ -5,4 +5,6 @@
 #include "boost/smart_ptr.hpp"
 #include "boost/pool/pool.hpp"
 #include "boost/circular_buffer.hpp"
-#include "boost/optional.hpp"
+#include "boost/geometry/index/rtree.hpp"
+#include "boost/geometry.hpp"
+

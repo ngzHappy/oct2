e3c4cc5e74dc5de61175215fe8bb6ce13ad2c199
ngzHappy <819869472@qq.com>
2016-04-19 18:10:47 +0800

    +warp affine

--- opencv_image_transformations/opencv_warpAffine/opencv_warpAffine_run.cpp
+++ opencv_image_transformations/opencv_warpAffine/opencv_warpAffine_run.cpp
@@ -5,36 +5,48 @@
 #include "private/opencv_warpAffine_run_exception.cpp"
 //#include <QtCharts>
 
-namespace opencv_warpAffine{
-extern void run(OpenCVWindow * window) try{
+namespace opencv_warpAffine {
+extern void run(OpenCVWindow * window) try {
 
-    /*测试图片显示*/
-    {
-        intptr_t count_=0;
-        const auto images_names=
-            CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
+    intptr_t count_=0;
+    const auto images_names=
+        CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
 
-            for (const auto & image_name:images_names) {
-            window->insertImage(QImage(image_name))
-                ->setWindowTitle(u8"第%1幅图片"_qs.arg(++count_));
-        }
-    }
-    /*测试柱状图*/
-    window->insertHist({ 1,2,3,4,5 })->setWindowTitle(u8"柱状图"_qs);
-    /*测试散点图*/
-    auto scatter=window->insertScatter({ {0,0},{1,1},{2,2} });
-    scatter->setCentrePointPaint(
-        std::shared_ptr< std::function<void(QPainter *)> >(
-        new std::function<void(QPainter *)>{
-        [](QPainter * painter) {
-        painter->setBrush(Qt::transparent);
-        painter->setPen(QPen(QColor(0,0,0),1));
-        painter->drawRect(QRect{-10,-10,20,20});
-    }
-    }
-        )
+    for (const auto & image_name:images_names) {
+        QImage imageInput=QImage(image_name);
+        window->insertImage(imageInput)
+            ->setWindowTitle(u8"第%1幅图片"_qs.arg(++count_));
+
+        auto imageHeigth=imageInput.height();
+        auto imageWidth=imageInput.width();
+
+        std::vector< cv::Point2f > varFrom{
+            cv::Point2f(0,0),
+            cv::Point2f(0,imageHeigth),
+            cv::Point2f(imageWidth,0),
+        };
+
+        std::vector< cv::Point2f > varTo{
+            cv::Point2f(0,imageHeigth/4.0),
+            cv::Point2f(imageWidth/4.0,imageHeigth),
+            cv::Point2f(imageWidth/4.0*3,0),
+        };
+
+        cv::Mat matrixAffineTransform = cv::getAffineTransform(varFrom,varTo);
+
+        cv::Mat ans;
+        cv::Mat image=OpenCVUtility::tryRead(imageInput);
+        cv::warpAffine(
+            image,
+            ans,
+            matrixAffineTransform,
+            image.size()
         );
-    scatter->setWindowTitle(u8"散点图"_qs);
+
+        window->insertImage(ans)
+            ->setWindowTitle(u8"第%1幅图片结果"_qs.arg(count_));
+
+    }
 
 }
 catch (const cv::Exception &e) {

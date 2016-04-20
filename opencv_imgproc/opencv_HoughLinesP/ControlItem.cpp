e45785c8ebc064865271d141f5e420de4b6e9c04
ngzHappy <819869472@qq.com>
2016-04-20 18:46:16 +0800

    +files

---
+++ opencv_imgproc/opencv_HoughLinesP/ControlItem.cpp
@@ -0,0 +1,24 @@
+﻿#include "ControlItem.hpp"
+#include "ui_ControlItem.h"
+
+ControlItem::ControlItem(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ControlItem)
+{
+    ui->setupUi(this);
+}
+
+ControlItem::~ControlItem()
+{
+    delete ui;
+}
+
+void ControlItem::on_do_button_clicked()
+{
+
+}
+
+void ControlItem::on_select_image_button_clicked()
+{
+
+}
---
+++ opencv_imgproc/opencv_HoughLinesP/ControlItem.hpp
@@ -0,0 +1,27 @@
+﻿#ifndef CONTROLITEM_HPP
+#define CONTROLITEM_HPP
+
+#include <QtWidgets/QWidget>
+
+namespace Ui {
+class ControlItem;
+}
+
+class ControlItem : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ControlItem(QWidget *parent = 0);
+    ~ControlItem();
+
+private slots:
+    void on_do_button_clicked();
+
+    void on_select_image_button_clicked();
+
+private:
+    Ui::ControlItem *ui;
+};
+
+#endif // CONTROLITEM_HPP

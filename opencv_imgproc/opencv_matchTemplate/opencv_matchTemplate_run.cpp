0f7a5bfa80a7b49d5048534a09a193c096e88581
ngzHappy <819869472@qq.com>
2016-04-20 12:55:15 +0800

    +ui change

--- opencv_imgproc/opencv_matchTemplate/opencv_matchTemplate_run.cpp
+++ opencv_imgproc/opencv_matchTemplate/opencv_matchTemplate_run.cpp
@@ -4,11 +4,17 @@
 #include <QtCore/qdebug.h>
 #include "private/opencv_matchTemplate_run_exception.cpp"
 #include "OpenCVWindowDetail.hpp"
+#include "ControlItem.hpp"
 //#include <QtCharts>
 
 OpenCVImageItem * OpenCVWindowDetail::insertImage(QImage inputImage) {
     OpenCVImageItem * ans =OpenCVWindow::insertImage(inputImage);
-
+    auto * var_vi= new OpenCVVerticalItems(ans);
+    var_vi->addWidget(new ControlItem);
+    ans->resize(
+        inputImage.width()+360,
+        inputImage.height()+360
+    );
     return ans;
 }
 
@@ -17,10 +23,10 @@ extern void run(OpenCVWindow * window) try {
 
     const auto images_names=
         CoreUtility::getConfigurationFile().getInputImagesNames("images:000001");
-
+    std::intptr_t count_=0;
     for (const auto & image_name:images_names) {
         window->insertImage(QImage(image_name))
-            ->setWindowTitle(u8"原图"_qs);
+            ->setWindowTitle(u8"%1"_qs.arg(++count_));
     }
 
 }

53bf321d108a57800146175f19e49322c4e07750
ngzHappy <819869472@qq.com>
2016-04-21 11:58:44 +0800

    +hls

--- opencv_cvtColor/opencv_2HLS/opencv_2HLS_run.cpp
+++ opencv_cvtColor/opencv_2HLS/opencv_2HLS_run.cpp
@@ -8,7 +8,7 @@
 #include <QtWidgets/qfiledialog.h>
 //#include <QtCharts>
 OpenCVImageItem * OpenCVWindowDetail::insertImage(QImage i){
-    auto ans=OpenCVWindow::insertImage(i.convertToFormat(QImage::Format_RGB888));
+    auto ans=OpenCVWindow::insertImage(i.convertToFormat(QImage::Format_RGBA8888));
     auto item=new OpenCVVerticalItems(ans);
     item->addWidget(new ControlItem(ans),true);
     ans->resize(768,600);
@@ -33,20 +33,50 @@ void ControlItem::on_do_button_clicked(){
         if (inputImage.isNull()) { return{}; }
         try {
             cv::Mat image=OpenCVUtility::tryRead(
-                inputImage.convertToFormat(QImage::Format_RGB888));
-            image.convertTo(image,CV_32FC3);
+                inputImage.convertToFormat(QImage::Format_RGBA8888));
+            image.convertTo(image,CV_32FC4);
+
+            /*获得rgba,用于保存alpha*/
+            std::vector<cv::Mat> rgba{4};
+            cv::split(image,rgba);
+            {
+                std::vector<cv::Mat> rgb{rgba[0],rgba[1],rgba[2]};
+                cv::merge(rgb,image);
+                rgba[0].release();
+                rgba[1].release();
+                rgba[2].release();
+            }
+
+            /*转换到hls空间*/
             cv::cvtColor(image,image,cv::COLOR_RGB2HLS);
-            std::vector<cv::Mat> hls{3};
+            
+            std::vector<cv::Mat> hls;
+            hls.reserve(4);
             cv::split(image,hls);
+            image.release();
+
+            /*对hls空间进行线性变换*/
             hls[0]*=pack->h; 
             hls[1]*=pack->l;
             hls[2]*=pack->s;
+
             if(pack->hBase!=0)hls[0]+=pack->hBase; 
             if(pack->lBase!=0)hls[1]+=pack->lBase;
             if(pack->sBase!=0)hls[2]+=pack->sBase;
+
+            /*将hls重新转换为rgb*/
             cv::merge(hls,image);
             cv::cvtColor(image,image,cv::COLOR_HLS2RGB);
+            
+            /*将rgb+a组成rgba*/
+            cv::split(image,hls);
+            hls.push_back(rgba[3]);
+            cv::merge(hls,image);
+
+            /*清空无用数据*/
             hls.clear();
+            rgba.clear();
+
             return OpenCVUtility::tryRead(image);
         }
         catch (const cv::Exception &e) {

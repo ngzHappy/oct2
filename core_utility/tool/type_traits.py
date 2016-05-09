08250831b63eccaed233c4096046685dc87d9904
ngzHappy <819869472@qq.com>
2016-05-09 17:29:21 +0800

    +tool

--- core_utility/tool/type_traits.py
+++ core_utility/tool/type_traits.py
@@ -2,6 +2,20 @@
 
 def get_type_traits():
     return"""/*type traits*/
+#ifndef __PRAGMA_ONCE____SSS___HXX_0x00
+#define __PRAGMA_ONCE____SSS___HXX_0x00
+
+#include <memory>
+#include <cstdlib>
+#include <utility>
+#include <cstddef>
+#include <cstdint>
+#include <iterator>
+#include <type_traits>
+
+/*zone_namespace_begin*/
+
+class ___SSS___TypeTraits;
 class ___SSS___TypeTraits {
 public:
     template<typename...>using void_t=void;
@@ -9,23 +23,31 @@ public:
     using Integer=std::int32_t;
     template<typename __T__>
     using SharedPointer=std::shared_ptr<__T__>;
-    ___SSS___TypeTraits()=default;
+    template<typename __T__>
+    using WeakPointer=std::weak_ptr<__T__>;
+    template<typename __T__>
+    using SharedFromThis=std::enable_shared_from_this<__T__>;
     template<typename __T__,typename ...__A__>
     static SharedPointer<__T__> make_shared(__A__&&..._a_) {
         return SharedPointer<__T__>(
             new __T__(std::forward<__A__>(_a_)...),
             [](__T__*arg) {delete arg; });}
+    ___SSS___TypeTraits()=default;
 protected:
     virtual ~___SSS___TypeTraits()=default;
     ___SSS___TypeTraits(const ___SSS___TypeTraits&)=delete;
     ___SSS___TypeTraits&operator=(const ___SSS___TypeTraits&)=delete;
     ___SSS___TypeTraits(___SSS___TypeTraits&&)=delete;
     ___SSS___TypeTraits&operator=(___SSS___TypeTraits&&)=delete;
-};"""
+};
+
+/*zone_namespace_end*/
+
+#endif"""
     pass
 
 if __name__=="__main__":
-    ans=get_type_traits().replace("___SSS___","ABC");
+    ans=get_type_traits().replace("___SSS___","___SSS___");
     with open("ABC.hpp","w") as fp:
         fp.write(ans);
         fp.write("\r\n");
